52bb10a32ae0711bf1605862077bc18efe67dac7
Arun Unnikrishnan <92980954+arun-t-u@users.noreply.github.com>
2025-04-15 18:50:52 +0530

    Update routes.py

--- apps/modules/auth/routes.py
+++ apps/modules/auth/routes.py
@@ -1,7 +1,6 @@
 from flask import Blueprint, request, jsonify, current_app
 from werkzeug.security import check_password_hash
 from flask_jwt_extended import create_access_token, create_refresh_token, jwt_required, get_jwt_identity
-from apps.modules.models import User, StatusEnum, Institution
 
 
 auth_bp = Blueprint('auth', __name__, url_prefix='/auth')
