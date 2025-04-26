4b808b8d8c7863ca6ef945aa2de929f09ac8637e
Arun Unnikrishnan <92980954+arun-t-u@users.noreply.github.com>
2025-04-26 11:51:04 +0530

    Update routes.py

--- apps/modules/auth/routes.py
+++ apps/modules/auth/routes.py
@@ -6,6 +6,11 @@ from flask_jwt_extended import create_access_token, create_refresh_token, jwt_re
 auth_bp = Blueprint('auth', __name__, url_prefix='/auth')
 
 
+@auth_bp.route('/api', methods=['GET'])
+def get_message():
+    return jsonify({'message': 'Hello from Flask!'})
+
+
 @auth_bp.route('/test', methods=['POST'])
 def test():
     return jsonify({"message": "Test successful"}), 200
